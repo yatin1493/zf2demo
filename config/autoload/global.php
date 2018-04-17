49a032b7d568440ec808b2ce7c678d7ca1e3a399
innovation <innovate123@yahoo.com>
2018-04-17 09:10:14 +0000

    Album module added to z2

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,6 +11,17 @@
  * file.
  */
 
-return array(
-    // ...
-);
+return [
+    'db' => array(
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=demo;host=localhost',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+         )
+     ),
+     'service_manager' => array(
+         'factories' => array(
+             'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory'
+          )
+     )
+];
